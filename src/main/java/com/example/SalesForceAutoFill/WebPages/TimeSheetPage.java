3019b2497a6d7557d4ad31155e77d23d0a63450c
Kwame Marfo <kwame.marfo@fdmgroup.com>
2022-02-21 12:52:25 +0000

    Cleaned up

--- src/main/java/com/example/SalesForceAutoFill/WebPages/TimeSheetPage.java
+++ src/main/java/com/example/SalesForceAutoFill/WebPages/TimeSheetPage.java
@@ -46,20 +46,31 @@ public class TimeSheetPage {
 	@Value("${Location}")
 	private String locationTimeSheet;
 	
-
-	public boolean checkHours(String tabHours) {
-		if (tabHours.equals("0.00")) {
-			return true;
-		}
-		return false;
-	}
-
+	private By dateBy = By.cssSelector(".breadcrumb-item-link.breadcrumb-heading");
+	
+	private By tabHeaderBy = By.xpath("//div[@id='fixed-header-container']");
+	
+	private By tabElementsBy = By.xpath(".//div[contains("
+			+ "@class,'time-entry-period day-container no-text-select BusDay')]");
+	
+	private By submitAllBy = By.xpath("//li[@id='submit-all']");
+	
+	private By tabDateBy = By.className("time-entry-period-details");
+	
+	private By tabHourBy = By.className("time-entry-period-summary");
+	
+	private By newEntryBy = By.xpath(".//div["
+			+ "@class='time-entry-period-header-btn new-time-entry-btn hover-btn']");
+	
+	
 	public Boolean fillInPage(WebDriver driver) {
 		WebElement getDateFromWebsite = new WebDriverWait(driver, (15)).until(
-				driver2 -> driver.findElement(By.cssSelector(".breadcrumb-item-link.breadcrumb-heading")));
+				driver2 -> driver.findElement(dateBy));
 
-		String startDateOfTimesheet = getDateFromWebsite.getText().split(Pattern.quote("("))[1].split(" ")[0];
-		LocalDate currentSheetDate = dateServices.formatDate("secondDF", startDateOfTimesheet);
+		String startDateOfTimesheet = getDateFromWebsite.getText()
+				.split(Pattern.quote("("))[1].split(" ")[0];
+		LocalDate currentSheetDate = dateServices
+				.formatDate("secondDF", startDateOfTimesheet);
 		Boolean isCurrentWeekSheet = dateServices.checkDate(currentSheetDate);
 		Boolean noProblemFillingTimeSheet = true;
 		
@@ -67,14 +78,12 @@ public class TimeSheetPage {
 			pageManipulate.switchFrame(driver, 1);
 			String monthAndYear = startDateOfTimesheet.replaceAll("^\\d+", "");
 			WebElement websiteTabHeader = new WebDriverWait(driver, 15).until(
-					ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@id='fixed-header-container']")));
-
-			List<WebElement> tabElements = websiteTabHeader.findElements(
-					By.xpath(".//div[contains(@class,'time-entry-period day-container no-text-select BusDay')]"));
+					ExpectedConditions.visibilityOfElementLocated(tabHeaderBy));
+			List<WebElement> tabElements = websiteTabHeader.findElements(tabElementsBy);
 
 			String checkSubmited;
 			try {
-				checkSubmited = driver.findElement(By.xpath("//li[@id='submit-all']")).getAttribute("class");
+				checkSubmited = driver.findElement(submitAllBy).getAttribute("class");
 			} catch (NoSuchElementException e2) {
 				e2.printStackTrace();
 				noProblemFillingTimeSheet = false;
@@ -83,43 +92,44 @@ public class TimeSheetPage {
 
 			for (WebElement tabElement : tabElements) {
 				// check date from tab
-				WebElement tabDate = tabElement.findElement(By.className("time-entry-period-details"));
+				WebElement tabDate = tabElement.findElement(tabDateBy);
 				String fulltabDate = tabDate.getText().split(" ")[1] + monthAndYear;
-				LocalDate tabDateFormated = dateServices.formatDate("secondDF", fulltabDate);
+				LocalDate tabDateFormated = dateServices
+						.formatDate("secondDF", fulltabDate);
 				Boolean checktabDateFormated = dateServices.checkDate(tabDateFormated);
 
 				// check if tab has any values for hours
-				WebElement tabHourElement = tabElement.findElement(By.className("time-entry-period-summary"));
+				WebElement tabHourElement = tabElement.findElement(tabHourBy);
 				String tabHours = tabHourElement.getText().split("\\Hours")[0];
 				boolean checktabHours = this.checkHours(tabHours);
 
-				if (checktabDateFormated && checktabHours && checkSubmited.equals("disabled")) {
-
+				if (checktabDateFormated && checktabHours && 
+						checkSubmited.equals("disabled")) {			
 					driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
-					WebElement element = tabElement.findElement(
-							By.xpath(".//div[@class='time-entry-period-header-btn new-time-entry-btn hover-btn']"));
-
+					WebElement newEntrybtn = tabElement.findElement(newEntryBy);
+					
 					Actions act = new Actions(driver);
-					act.moveToElement(element).click().perform();
+					act.moveToElement(newEntrybtn).click().perform();
 
+					//Fill in each new entry (time-sheet)
 					fillInPage.fillInPage(driver);
 				}
 				System.out.println("<--  TimeSheet Completed -->: ");
 			}
-
-			String checkSubmited1 = driver.findElement(By.xpath("//li[@id='submit-all']")).getAttribute("class");
-
+			
+			String checkSubmited1 = driver.findElement(submitAllBy)
+					.getAttribute("class");
 			if (checkSubmited1.equals("disabled")) {
-				//BREAK LOOP HERE and LOG OUT, BREAKING LOOP ---
+				//BREAK LOOP HERE and LOG OUT ---
 				break;
 			}
 
-			// Page is reloading, checkSubmited to see what is happening
 			while (!checkSubmited1.equals("disabled")) {
 				try {
 					Thread.sleep(10000);
-					System.out.println("---- Waiting for user to submit Timesheet ----------");
-					checkSubmited1 = driver.findElement(By.xpath("//li[@id='submit-all']")).getAttribute("class");
+					System.out.println("---- Waiting for user to submit Timesheet ---");
+					checkSubmited1 = driver.findElement(submitAllBy)
+							.getAttribute("class");
 					if (checkSubmited1.equals("disabled")) {
 						System.out.println("User submitted TimeSheets");
 						nextTSPage.nextSheet(driver);
@@ -133,5 +143,12 @@ public class TimeSheetPage {
 			}
 		} while (isCurrentWeekSheet);
 		return noProblemFillingTimeSheet;
+	}
+	
+	private boolean checkHours(String tabHours) {
+		if (tabHours.equals("0.00")) {
+			return true;
+		}
+		return false;
 	}
 }
